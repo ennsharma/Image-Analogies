f3246ddbd915dd5b767d8f5a5cd7c15c56453bbc
Nikhil Sharma <ennsharma@berkeley.edu>
2017-12-08 18:36:37 -0800

    Finished working implementation

--- image_analogies.py
+++ image_analogies.py
@@ -4,12 +4,9 @@ import skimage.io as skio
 from skimage.transform import rescale
 from sklearn.neighbors import LSHForest
 
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-
 # Hyperparameters
 L = 3
-K = 3
+K = 0.5
 
 # Kernels
 GAUSSIAN_SMALL = np.array([[1, 2, 1],
@@ -28,62 +25,66 @@ GAUSSIAN_LARGE = np.array([[1, 4, 6, 4, 1],
 INPUT = './input/'
 OUTPUT = './output/'
 
-A_NAME = 'C.jpg'
-A_PRIME_NAME = 'C_prime.jpg'
-B_NAME = 'D.jpg'
-B_PRIME_NAME = 'D_prime.jpg'
+A_NAME = 'A.jpg'
+A_PRIME_NAME = 'A_prime.jpg'
+B_NAME = 'B.jpg'
+B_PRIME_NAME = 'B_prime.jpg'
 
 # Feature Vector Storage
 vector_cache = {}
 lshf_cache = {}
 
 # Feature Extraction
-def get_features_and_pixels(i, j, offset_size, l, pyramid, kernel):
-    features, pixels = [], []
+def get_features_and_offsets(i, j, offset_size, l, pyramid, kernel):
+    features, offsets = [], []
     for i_offset in range(-offset_size, offset_size+1):
         for j_offset in range(-offset_size, offset_size+1):
             i_prime, j_prime = i + i_offset, j + j_offset
             if i_prime >= 0 and i_prime < pyramid[l].shape[0] and j_prime >= 0 and j_prime < pyramid[l].shape[1] and np.sum(pyramid[l][i_prime][j_prime]):
                 weight = kernel[i_offset+offset_size][j_offset+offset_size]
                 features.append((pyramid[l][i_prime][j_prime], weight))
-                pixels.append((i_prime, j_prime, weight))
-    return features, pixels
+                offsets.append((i_offset, j_offset, weight))
+    return features, offsets
 
-def get_features_only(pixels, l, pyramid):
+def get_features_only(i, j, offsets, l, pyramid):
     features = []
-    for i_prime, j_prime, weight in pixels:
-        features.append((pyramid[l][i_prime][j_prime], weight))
+    for i_offset, j_offset, weight in offsets:
+        i_prime, j_prime = i + i_offset, j + j_offset
+        if i_prime >= 0 and i_prime < pyramid[l].shape[0] and j_prime >= 0 and j_prime < pyramid[l].shape[1]:
+            features.append((pyramid[l][i_prime][j_prime], weight))
+        else:
+            features.append((np.array([0, 0, 0]), 0))
     return features
 
-def construct_F(i, j, l, pyramid, pyramid_prime, pixels=None, is_A=True):
-    if pixels:
-        size = sum([len(x) for x in pixels])
+def construct_F(i, j, l, pyramid, pyramid_prime, offsets=None, is_A=True):
+    if offsets:
+        size = sum([len(x) for x in offsets])
         if (i, j, l, is_A, size) in vector_cache:
-            return vector_cache[(i, j, l, is_A)]
-        high_res_bp_features = get_features_only(pixels[0], l, pyramid_prime)
-        high_res_b_features = get_features_only(pixels[1], l, pyramid)
+            return vector_cache[(i, j, l, is_A, size)]
+        high_res_bp_features = get_features_only(i, j, offsets[0], l, pyramid_prime)
+        high_res_b_features = get_features_only(i, j, offsets[1], l, pyramid)
         if l+1 != L:
-            low_res_bp_features = get_features_only(pixels[2], l-1, pyramid_prime)
-            low_res_b_features = get_features_only(pixels[3], l-1, pyramid_prime)
+            low_res_bp_features = get_features_only(i, j, offsets[2], l-1, pyramid_prime)
+            low_res_b_features = get_features_only(i, j, offsets[3], l-1, pyramid_prime)
             features = high_res_bp_features + high_res_b_features + low_res_bp_features + low_res_b_features
         else:
             features = high_res_bp_features + high_res_b_features
         vector_cache[(i, j, l, is_A)] = features
         return features
     else:
-        high_res_bp_features, high_res_bp_pixels = get_features_and_pixels(i, j, 2, l, pyramid_prime, GAUSSIAN_LARGE)
-        high_res_b_features, high_res_b_pixels = get_features_and_pixels(i, j, 2, l, pyramid, GAUSSIAN_LARGE)
+        high_res_bp_features, high_res_bp_offsets = get_features_and_offsets(i, j, 2, l, pyramid_prime, GAUSSIAN_LARGE)
+        high_res_b_features, high_res_b_offsets = get_features_and_offsets(i, j, 2, l, pyramid, GAUSSIAN_LARGE)
         if l+1 != L:
-            low_res_bp_features, low_res_bp_pixels = get_features_and_pixels(i//2, j//2, 1, l-1, pyramid_prime, GAUSSIAN_SMALL)
-            low_res_b_features, low_res_b_pixels = get_features_and_pixels(i//2, j//2, 1, l-1, pyramid, GAUSSIAN_SMALL)
+            low_res_bp_features, low_res_bp_offsets = get_features_and_offsets(i//2, j//2, 1, l-1, pyramid_prime, GAUSSIAN_SMALL)
+            low_res_b_features, low_res_b_offsets = get_features_and_offsets(i//2, j//2, 1, l-1, pyramid, GAUSSIAN_SMALL)
             features = high_res_bp_features + high_res_b_features + low_res_bp_features + low_res_b_features
-            pixels = [high_res_bp_pixels, high_res_b_pixels, low_res_bp_pixels, low_res_b_pixels]
+            offsets = [high_res_bp_offsets, high_res_b_offsets, low_res_bp_offsets, low_res_b_offsets]
         else:
             features = high_res_bp_features + high_res_b_features
-            pixels = [high_res_bp_pixels, high_res_b_pixels]
-        size = sum([len(x) for x in pixels])
+            offsets = [high_res_bp_offsets, high_res_b_offsets]
+        size = sum([len(x) for x in offsets])
         vector_cache[(i, j, l, is_A, size)] = features
-        return features, pixels
+        return features, offsets
 
 # Construction Methods
 def construct_pyramid(im):
@@ -101,14 +102,14 @@ def construct_normalized_vector(F, feature_size=3):
         i = i + feature_size
     return feature_vector / np.linalg.norm(feature_vector), weights
 
-def construct_lshf(pyramid, prime_pyramid, l, query, pixels):
+def construct_lshf(pyramid, prime_pyramid, l, query, offsets):
     if (l, query.shape) in lshf_cache:
         return lshf_cache[(l, query.shape)]
 
     X_train, idx_map, idx = [], {}, 0
     for i_a in range(prime_pyramid[l].shape[0]):
         for j_a in range(prime_pyramid[l].shape[1]):
-            features = construct_F(i_a, j_a, l, pyramid, prime_pyramid, pixels=pixels)
+            features = construct_F(i_a, j_a, l, pyramid, prime_pyramid, offsets=offsets)
             neighbor = construct_normalized_vector(features)[0]
             if neighbor.shape == query.shape:
                 X_train.append(neighbor)
@@ -137,11 +138,10 @@ def compute_distance(F_1, F_2):
 # Texture Synthesis
 def best_approximate_match(A_pyramid, A_prime_pyramid, B_pyramid, B_prime_pyramid, s, l, i_b, j_b):
     # Construct feature vectors
-    features, pixels = construct_F(i_b, j_b, l, B_pyramid, B_prime_pyramid, is_A=False)
+    features, offsets = construct_F(i_b, j_b, l, B_pyramid, B_prime_pyramid, is_A=False)
     query = construct_normalized_vector(features)[0]
-    lshf, X_train, idx_map = construct_lshf(A_pyramid, A_prime_pyramid, l, query, pixels)
+    lshf, X_train, idx_map = construct_lshf(A_pyramid, A_prime_pyramid, l, query, offsets)
     _, indices = lshf.kneighbors([query], n_neighbors=1)
-    # print(indices)
     return idx_map[indices[0][0]]
 
 def best_coherence_match(A_pyramid, A_prime_pyramid, B_pyramid, B_prime_pyramid, s, l, i, j):
@@ -152,8 +152,8 @@ def best_coherence_match(A_pyramid, A_prime_pyramid, B_pyramid, B_prime_pyramid,
             if i_prime >= 0 and i_prime < B_prime_pyramid[l].shape[0] and j_prime >= 0 and j_prime < B_prime_pyramid[l].shape[1] and (i_prime, j_prime) in s:
                 x, y = i - i_prime + s[(i_prime, j_prime)][0], j - j_prime + s[(i_prime, j_prime)][1]
                 if x >= 0 and x < B_prime_pyramid[l].shape[0] and y >=0 and y < B_prime_pyramid[l].shape[1]:
-                    F_1, pixels = construct_F(x, y, l, A_pyramid, A_prime_pyramid)
-                    F_2 = construct_F(i, j, l, B_pyramid, B_prime_pyramid, pixels=pixels, is_A=False)
+                    F_1, offsets = construct_F(x, y, l, A_pyramid, A_prime_pyramid)
+                    F_2 = construct_F(i, j, l, B_pyramid, B_prime_pyramid, offsets=offsets, is_A=False)
                     norm = compute_distance(F_1, F_2)
                     if norm < min_norm:
                         best_i_prime, best_j_prime, min_norm = i_prime, j_prime, norm
@@ -166,23 +166,18 @@ def best_match(A_pyramid, A_prime_pyramid, B_pyramid, B_prime_pyramid, s, l, i_b
     i_a_app, j_a_app = best_approximate_match(A_pyramid, A_prime_pyramid, B_pyramid, B_prime_pyramid, s, l, i_b, j_b)
     i_a_coh, j_a_coh = best_coherence_match(A_pyramid, A_prime_pyramid, B_pyramid, B_prime_pyramid, s, l, i_b, j_b)
     if (i_a_coh, j_a_coh) == (None, None):
-        # print('approximate returned')
         return i_a_app, j_a_app
 
     # Compute neighborhood feature vector distances
-    F_q, pixels = construct_F(i_b, j_b, l, B_pyramid, B_prime_pyramid, is_A=False)
-    F_app = construct_F(i_a_app, j_a_app, l, A_pyramid, A_prime_pyramid, pixels=pixels)
-    F_coh = construct_F(i_a_coh, j_a_coh, l, A_pyramid, A_prime_pyramid, pixels=pixels)
+    F_q, offsets = construct_F(i_b, j_b, l, B_pyramid, B_prime_pyramid, is_A=False)
+    F_app = construct_F(i_a_app, j_a_app, l, A_pyramid, A_prime_pyramid, offsets=offsets)
+    F_coh = construct_F(i_a_coh, j_a_coh, l, A_pyramid, A_prime_pyramid, offsets=offsets)
 
     d_app = compute_distance(F_app, F_q)
     d_coh = compute_distance(F_coh, F_q)
-    # print("APPROXIMATE: %f" %d_app, i_a_app, j_a_app)
-    # print("COHERENCE: %f" %d_coh, i_a_coh, j_a_coh)
 
     if d_coh > d_app * (1 + 2**(L - l) * K):
-        # print('approximate returned')
         return i_a_app, j_a_app
-    # print('coherence returned')
     return i_a_coh, j_a_coh
 
 def create_image_analogy(A, A_prime, B):
@@ -208,12 +203,6 @@ if __name__ == '__main__':
     A = plt.imread(INPUT + A_NAME)
     A_prime = plt.imread(INPUT + A_PRIME_NAME)
     B = plt.imread(INPUT + B_NAME)
-    # plt.imshow(B)
-    # plt.show()
-    print (B.shape)
     B_prime = create_image_analogy(A, A_prime, B)
-
-    plt.imshow(B_prime)
-    plt.show()
-    skio.imsave(OUTPUT + B_PRIME_NAME + "SKIO.jpg", B_prime/255.)
+    
     plt.imsave(OUTPUT + B_PRIME_NAME, B_prime/255.)
